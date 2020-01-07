bc8ee79041dbf70160290a02e81a1a23142b4935
AJ <arunjagadishsd@gmail.com>
2020-01-07 16:53:07 +0530

    Added missing columns

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import dateutil.parser
 import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, ForeignKey, ARRAY
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
@@ -40,6 +40,10 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(500))
+    genres = db.Column(ARRAY(String))
+    genres = db.Column(ARRAY(db.String))
+    shows = db.relationship('Show', backref='Venue', lazy=True)
 
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
@@ -55,11 +59,21 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    seeking_description = db.Column(db.String(500))
+    website = db.Column(db.String(500))
+    phone = db.Column(db.String(120))
+    genres = db.Column(ARRAY(db.String))
+    shows = db.relationship('Show', backref='Artist', lazy=True)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
-
+class Show(db.Model):
+  __tablename__ = 'Show'
+  id = db.Column(db.Integer, primary_key=True)
+  venue_id = db.Column(db.Integer, ForeignKey(Venue.id))
+  artist_id = db.Column(db.Integer, ForeignKey(Artist.id))
+  show_time = db.Column(db.DateTime, nullable=False)
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
